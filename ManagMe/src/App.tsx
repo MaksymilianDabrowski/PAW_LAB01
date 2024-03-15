5c013735e9182ac909c725f84bdc0f38079fa665
Maksymilian <maksymilian.dabrowski421@gmail.com>
2024-03-15 13:11:46 +0100

    first commit

---
+++ ManagMe/src/App.tsx
@@ -0,0 +1,19 @@
+
+/* TODO: CRUD do zarządzania projektami - dodać 4 nowe komponenty 
+  Główny widok - App - ui https://ui.shadcn.com/docs/components/data-table
+  Create - id, nazwa, opis - ui https://ui.shadcn.com/docs/components/card
+  Read - pod główny widok 
+  Update - dropdown w głównym widoku - menu jako card
+  Delete - dropdown w głównym widoku 
+*/
+
+
+export default function App() {
+  return (
+    <>
+      <main>
+        Hello
+      </main>
+    </>
+  )
+}
